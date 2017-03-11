1ffc5062b3142e02c08d9dcbc9a139809d7ae74f
Artem Govorov <artem.govorov@gmail.com>
2017-03-11 19:50:14 +1000

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -1,32 +1,33 @@
 process.env.NODE_ENV = 'test';
 
 const path = require('path');
-const fs = require('fs');
-const wallabyWebpack = require('wallaby-webpack');
-
-const webpackConfig = require('./build/webpack.test.conf');
-const _babelConfig = fs.readFileSync(path.join(__dirname, '.babelrc'), 'utf-8');
-const babelConfig = JSON.parse(_babelConfig);
 
 module.exports = function (wallaby) {
-  const postProcessor = wallabyWebpack(webpackConfig);
+  process.env.NODE_PATH += path.delimiter + path.join(wallaby.projectCacheDir, 'src') +
+    path.delimiter + path.join(wallaby.projectCacheDir);
 
   return {
     debug: true,
     testFramework: 'mocha',
     files: [
-      { pattern: 'node_modules/react/dist/react-with-addons.js', instrument: false },
-      { pattern: 'src/**/!(*-spec).js?(x)', load: false },
-      { pattern: 'src/**/!(*-storybook).js?(x)', ignore: true },
-      { pattern: 'test/unit/spec/helpers/**/*.js?(x)', instrument: false, load: false }
+      'src/**/*.js?(x)',
+      'src/**/*.scss',
+      'src/**/*.css',
+      'test/unit/spec/helpers/**/*.js?(x)',
+      '!src/**/*-spec.js?(x)',
+      '!src/**/*-storybook.js?(x)'
     ],
     tests: [
-      { pattern: 'src/**/*-spec.js?(x)', load: false }
+      'src/**/*-spec.js?(x)'
     ],
-    postprocessor: postProcessor,
+    env: {
+      type: 'node'
+    },
+    compilers: {
+      '**/*.js?(x)': wallaby.compilers.babel()
+    },
     setup: function () {
-      // required to trigger test loading
-      window.__moduleBundler.loadTests();
+      require.extensions['.css'] = require.extensions['.sass'] = () => {};
     }
   };
 };
