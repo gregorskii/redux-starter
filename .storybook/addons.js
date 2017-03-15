d798092b99a08f0d2c4f6e38bc5c7e4d1cf63f9c
Greg Parsons <gregorskii@gmail.com>
2017-03-15 14:59:33 -0700

    adds storybook redux-listener

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,3 +2,5 @@
 import '@kadira/storybook/addons';
 //  To add the knobs addon
 import '@kadira/storybook-addon-knobs/register';
+// Add the Redux Listener addon
+import 'storybook-addon-redux-listener/register';
--- src/store.js
+++ src/store.js
@@ -1,13 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createStorybookListener from 'storybook-addon-redux-listener';
 import promise from 'redux-promise';
 
 import Reducers from './reducers';
 
+const middlewares = [promise];
+
+if (process.env.NODE_ENV === 'storybook') {
+  const reduxListener = createStorybookListener();
+  middlewares.push(reduxListener);
+}
+
 const createStoreWithMiddleware = (reducers) => {
   return createStore(
     reducers,
-    composeWithDevTools(applyMiddleware(...[promise]))
+    composeWithDevTools(applyMiddleware(...middlewares))
   );
 };
 
