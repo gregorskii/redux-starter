736240fd6136c9bb8f45629d22d0b27d098eae50
Gregory Parsons <gregorskii@gmail.com>
2017-03-16 17:00:48 -0700

    Update webpack.base.conf.js
    fix loaders

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -25,7 +25,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loader: 'style!css!sass',
+        loader: 'style-loader!css-loader!sass-loader',
         exclude: /node_modules/
       }
     ]
