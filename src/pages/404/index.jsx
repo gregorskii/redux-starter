e4540ca627ae34a6d09695d4a03ccc03b859fff9
Greg Parsons <gregorskii@gmail.com>
2017-04-11 09:44:24 -0700

    adds 404 page

---
+++ src/pages/404/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FourOFour = () => {
+  return (
+    <section>
+      <h4>Thats a 404!</h4>
+    </section>
+  );
+};
+
+export default FourOFour;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import {
   Route,
-  BrowserRouter
+  BrowserRouter,
+  Switch
 } from 'react-router-dom';
 
 import App from 'components/app';
 import Home from 'pages/home';
+import FourOFour from 'pages/404';
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <App>
-        <Route exact path="/" component={Home} />
-        <Route path="/test" render={() => (<div>Test</div>)} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/test" render={() => (<div>Test</div>)} />
+          <Route component={FourOFour} />
+        </Switch>
       </App>
     </BrowserRouter>
   );
